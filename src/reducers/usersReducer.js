63011b0a81de44eed6d1777531d77baf3c78e64d
Emmanuel Nana Antwi <sneezy300@gmail.com>
2020-11-24 18:29:47 +0000

    add .

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -11,8 +11,7 @@ const initialState = {
 const usersReducer = (state = initialState, action) => {
     switch (action.type) {
         case "ADD_USER":
-                    return state;    
-            break;
+                    return state;
     
         default:
             return state;
