923390cfb487e907c136cf3d5a46046aebfb7d1e
Emmanuel Nana Antwi <sneezy300@gmail.com>
2021-01-23 11:14:04 +0000

    add firebase confiq

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,16 @@
-import {createStore} from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import usersReducer from '../reducers/usersReducer';
 
+//New imports
+import firebase from "../firebase/firebase"
+import { getFirebase, reactReduxFirebase } from "react-redux-firebase";
+import { getFirestore, reduxFirestore } from "redux-firestore";
+import thunk from "redux-thunk";
 
-export const store = createStore(usersReducer);
+export const store = createStore(usersReducer,
+    compose(
+        applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+        reactReduxFirebase(firebase),
+        reduxFirestore(firebase)
+    ));
 
