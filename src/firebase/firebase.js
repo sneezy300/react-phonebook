c2b5fd192928c0e0d0374729c7c723c6ec6bef10
Emmanuel Nana Antwi <sneezy300@gmail.com>
2021-01-23 10:01:15 +0000

    add firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyB7g7BsxZhr2j3vhTBVCes8PAV86I_lsNw",
+    authDomain: "user-react-77cab.firebaseapp.com",
+    projectId: "user-react-77cab",
+    storageBucket: "user-react-77cab.appspot.com",
+    messagingSenderId: "1012525729007",
+    appId: "1:1012525729007:web:93517996e8ef38bfa1471a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
